b76fcbaf22af9812b33c4fe4eb5874f9c9d65b32
nedarctic <nedarctic@gmail.com>
2026-05-25 19:12:24 +0300

    token states

--- src/auth/jwt-auth.guard.ts
+++ src/auth/jwt-auth.guard.ts
@@ -1,5 +1,37 @@
 import { AuthGuard } from "@nestjs/passport";
-import { Injectable } from "@nestjs/common";
+import { Injectable, UnauthorizedException } from "@nestjs/common";
 
 @Injectable()
-export class JwtAuthGuard extends AuthGuard('jwt') { }
+export class JwtAuthGuard extends AuthGuard('jwt') {
+
+    handleRequest(err, user, info, context) {
+        const request = context.switchToHttp().getRequest();
+
+        const authHeader = request.headers.authorization;
+
+        if (!authHeader) {
+            throw new UnauthorizedException({
+                error: 'NO_TOKEN',
+                message: 'Access token missing',
+            });
+        }
+
+        
+        if (info?.name === 'TokenExpiredError') {
+            throw new UnauthorizedException({
+                error: 'TOKEN_EXPIRED',
+                message: 'Access token expired',
+            });
+        }
+
+        
+        if (err || info || !user) {
+            throw new UnauthorizedException({
+                error: 'INVALID_TOKEN',
+                message: 'Invalid token',
+            });
+        }
+
+        return user;
+    }
+}
